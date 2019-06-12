bbb4cad0b8975853ea586fd85a0aef32fc52bb17
e <360833343@qq.com>
2019-06-12 18:25:05 +0800

    Initial commit

---
+++ core/src/main/java/com/zyx/core/security/service/CheckUser.java
@@ -0,0 +1,10 @@
+package com.zyx.core.security.service;
+
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+
+public class CheckUser {
+    public void check(){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+    }
+}
