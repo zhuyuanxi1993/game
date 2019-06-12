ffd27bdc2cfcc12fa3a89dc770bc4d5c649dd4df
扫把 <360833343@qq.com>
2019-06-12 23:58:56 +0800

    2313

--- core/src/main/java/com/zyx/core/security/service/CheckUser.java
+++ core/src/main/java/com/zyx/core/security/service/CheckUser.java
@@ -1,10 +1,26 @@
 package com.zyx.core.security.service;
 
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.subject.Subject;
+
 
 public class CheckUser {
     public void check(){
         ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        SecurityManager securityManager = shiroFilterFactoryBean.getSecurityManager();
+        SecurityUtils.setSecurityManager(securityManager);
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+        }
     }
 }
